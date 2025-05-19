79b874fdcad76874900eedfd2c136b7d99b2280d
Dobby <kukhahyeon@gmail.com>
2025-05-19 09:05:51 +0900

    searchbar 사용 layout 파일에 suspense 추가

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,11 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 import Searchbar from "@/components/search/searchbar";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Searchbar />
+      <Suspense>
+        <Searchbar />
+      </Suspense>
 
       {children}
     </>
